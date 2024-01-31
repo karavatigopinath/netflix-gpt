b3034ef175b5112f069921b6ee6f02928e110266
karavatigopinath <158138570+karavatigopinath@users.noreply.github.com>
2024-01-31 10:34:08 +0530

    firstcommit

---
+++ .history/src/App_20240131101147.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="text-center">
+       <div className="p-4 m-4 text-center">Hello</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
